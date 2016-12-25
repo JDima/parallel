e9b1e5f7678da5c16eec3f5289ec388b51ce71c2
Дмитрий Яковлев <JDima@Dmitrijs-MacBook-Pro.local>
2016-12-25 03:02:18 +0300

    Runnable MPI

--- univ/seidel/mpi/seidel.c
+++ univ/seidel/mpi/seidel.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #define ROOT 0
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
@@ -25,27 +26,9 @@ void printintx(int rank, int *x, int n)
   printf("\n");
 }
 
-int * scatterv(int nodes, int counts, int rem, double *sendbuf, double *recvbuf)
-{
-  int i, sum = 0;
-  int *sendcounts = malloc(sizeof(int) * nodes);
-  int *displs = malloc(sizeof(int) * nodes);
-  for(i = 0; i < nodes; i++){
-      sendcounts[i] = counts;
-      if (rem > 0) {
-          sendcounts[i]++;
-          rem--;
-      }
-      displs[i] = sum;
-      sum += sendcounts[i];
-  }
-  MPI_Scatterv(&sendbuf, sendcounts, displs, MPI_DOUBLE, &recvbuf, counts + 1, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
-  return sendcounts;
-}
 
 int main(int argc, char *argv[]) {
-  int n, nstrip;
-  int rank, nodes, rowIndex;
+  int n, rank, nodes, rowIndex;
   double start_time, end_time, tick;
   double *A, *a, *B, *b, *tmp, *xx, *x, *p;
   double temp, norm = 0, local_norm = 0;
@@ -56,29 +39,44 @@ int main(int argc, char *argv[]) {
 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &nodes);
-  printf("%i Nodes %i\n", nodes);
-  nstrip = n / nodes;
-  a = (double*)calloc((n * n) / nodes, sizeof(double));
-  b = (double*)calloc(nstrip, sizeof(double));
 
-  if(rank == ROOT){
+  if(rank == ROOT) {
     A = (double*)calloc(n * n, sizeof(double));
     B = (double*)calloc(n, sizeof(double));
     for(i = 0; i < n; i++) {
-        B[i] = n * sqrt(n) + n;
+        B[i] = i + 1;
         rowIndex = i * n;
         for(j = 0; j < n; j++)
-            A[rowIndex + j] = sqrt(n) + n * sqrt(n) * (i==j);
+            A[rowIndex + j] = sqrt(j + 1) + n * sqrt(n) * (i==j);
     }
   }
-  int* nodeMatSize = scatterv(nodes, (n * n) / nodes, (n * n) % nodes, A, a);
-  int* nodeVecSize = scatterv(nodes, nstrip, n % nodes, B, b);
-  int* nodeVecOffsets = (int*)calloc(nodes, sizeof(int));
   sum = 0;
+  int *sendcounts = malloc(sizeof(int) * nodes);
+  int *displs = malloc(sizeof(int) * nodes);
+  int rem = n % nodes;
   for(i = 0; i < nodes; i++){
-      nodeVecOffsets[i] = sum;
-      sum += nodeVecSize[i];
+      sendcounts[i] = n / nodes;
+      if (rem > 0) {
+          sendcounts[i]++;
+          rem--;
+      }
+      displs[i] = sum;
+      sum += sendcounts[i];
   }
+  b = (double*)calloc(sendcounts[rank], sizeof(double));
+  MPI_Scatterv(B, sendcounts, displs, MPI_DOUBLE, b, sendcounts[rank], MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+
+  int* nodeVecSize = (int*)calloc(nodes, sizeof(int));
+  int* nodeVecOffsets = (int*)calloc(nodes, sizeof(int));
+  memcpy(nodeVecSize, sendcounts, nodes * sizeof(int));
+  memcpy(nodeVecOffsets, displs, nodes * sizeof(int));
+
+  for(i = 0; i < nodes; i++){
+    sendcounts[i] *= n;
+    displs[i] *= n;
+  }
+  a = (double*)calloc(sendcounts[rank], sizeof(double));
+  MPI_Scatterv(A, sendcounts, displs, MPI_DOUBLE, a, sendcounts[rank], MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
 
   x = (double*)calloc(n, sizeof(double));
   for (i = 0; i < n; i++)
@@ -86,7 +84,7 @@ int main(int argc, char *argv[]) {
   start_time = MPI_Wtime();
   do
   {
-    local_norm = 0;
+    local_norm = 0.0;
     for (i = 0; i < nodeVecSize[rank]; i++)
     {
       temp = 0.0;
@@ -100,11 +98,11 @@ int main(int argc, char *argv[]) {
     }
 
     MPI_Allreduce(&local_norm, &norm, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    MPI_Allgatherv(MPI_IN_PLACE, nodeVecSize[rank], MPI_DOUBLE, &x[0], nodeVecSize, nodeVecOffsets, MPI_DOUBLE, MPI_COMM_WORLD);
+    MPI_Allgatherv(MPI_IN_PLACE, nodeVecSize[rank], MPI_DOUBLE, x, nodeVecSize, nodeVecOffsets, MPI_DOUBLE, MPI_COMM_WORLD);
 
     if(rank == ROOT)
       iter += 1;
-  } while (norm >= 1e-2);
+  } while (norm >= 1e-5);
   end_time = MPI_Wtime();
 
   MPI_Finalize();
